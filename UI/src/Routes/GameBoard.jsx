a919555cccaad55c4921f5069382e4c038aeec12
rotoforze <farmingwaresp@gmail.com>
2026-06-10 11:25:27 +0200

    juego

--- UI/src/Routes/GameBoard.jsx
+++ UI/src/Routes/GameBoard.jsx
@@ -1,6 +1,46 @@
+import {useState} from "react";
+import '../../public/game.css'
+import '../../public/game.css'
+
 function GameBoard() {
+
+    const [tablero, setTablero] = useState([
+        false, true, false,
+        false, false, false,
+        true, false, false
+    ]);
+
+    const [win, setWin] = useState(false);
+
+    const handleClick = (casilla) => {
+        const nuevoTablero = [...tablero];
+        const cols = 3;
+        const fila = Math.floor(casilla / cols);
+        const col = casilla % cols;
+
+        nuevoTablero[casilla] = !tablero[casilla];
+
+        // Arriba
+        if (fila - 1 >= 0) nuevoTablero[casilla - cols] = !tablero[casilla - cols];
+        // Abajo
+        if (fila + 1 < cols) nuevoTablero[casilla + cols] = !tablero[casilla + cols];
+        // Izquierda
+        if (col - 1 >= 0) nuevoTablero[casilla - 1] = !tablero[casilla - 1];
+        // Derecha
+        if (col + 1 < cols) nuevoTablero[casilla + 1] = !tablero[casilla + 1];
+
+        setTablero(nuevoTablero);
+    }
+
     return (
-        <>GameBoard</>
+        <>
+            {win && <h3>Has ganado</h3>}
+            <div className="tablero">
+                {tablero.map((activo, casilla) => {
+                    return <div key={casilla} className={"casilla btn "+ (activo ? 'btn-warning' : 'btn-dark')} onClick={() => {if (!win) handleClick(casilla)}}></div>
+                })}
+            </div>
+        </>
     )
 }
 
--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/index.esm.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
